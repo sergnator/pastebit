6beb1b8db7324c32e98843701673c1e5ac41848a
sergnator <86975413+sergnator@users.noreply.github.com>
2023-11-26 09:19:27 +0300

    Создание файла create.html

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+import sys
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/create', methods=['GET'])
+def create():
+    with open('html//create.html', encoding='utf-8') as f:
+        data = f.readlines()
+        data = ''.join(data)
+    return data.format(request)
+
+
+@app.route('/', methods=['GET'])
 def sample():
-    return 'был получен GET-запрос.'
+    with open('html//main.html', encoding='utf-8') as f:
+        data = f.read()
+    return data.format(request)
 
 
 if __name__ == '__main__':
